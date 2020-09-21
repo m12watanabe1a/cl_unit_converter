b9a86af5a758aaad28e7d3014c0e87134b3f32b5
m12watanabe1a <m12watanabe1a@gmail.com>
2020-09-21 18:22:01 +0900

    add functions

---
+++ c2k.zsh
@@ -0,0 +1,13 @@
+# Convert celsius to kelvin
+function c2k(){
+  source ./constants.zsh
+  if [[ "$1" = <-> ]]; then
+    let num=$1
+    let result=$(( num - K2C ))
+    printf "%.2f -> %.2f\n" $num $result
+    printf "%.2f" $result | pbcopy
+  else
+    echo "$1 is not a number"
+    return -1
+  fi
+}
---
+++ k2c.zsh
@@ -0,0 +1,15 @@
+# Convert kelvin to celsius
+function k2c(){
+  source ./constants.zsh
+  if [[ "$1" = <-> ]]; then
+    let num=$1
+    let result=$(( num + K2C ))
+    printf "%.2f -> %.2f\n" $num $result
+    printf "%.2f" $result | pbcopy
+  else
+    echo "$1 is not a number"
+    return -1
+  fi
+}
+
+
